5618a7e1788d4554d5477fcc353e72f356ba0316
jlego <273734893@qq.com>
2020-01-22 00:40:02 +0800

    no message

--- index.js
+++ index.js
@@ -4,12 +4,20 @@
  * by jlego on 2018-11-24
  */
 const cluster = require('cluster');
+const woodError = require('wood-error');
+const { Log, Util, error } = WOOD;
 
 module.exports = (app = {}, config = {}) => {
   // 返回错误信息
   app.application.use(function (err, req, res, next) {
     if (err) {
       res.status(err.status || 500);
+      let body = Util.getParams(req);
+      if (process.env.NODE_ENV && process.env.NODE_ENV === 'develope') console.log(err.stack || JSON.stringify(err));
+      if (!(err instanceof woodError.NoLogError)) {
+        Log.error(`path: ${req.path}, data: ${JSON.stringify(body.data)}, error:${err.stack || JSON.stringify(err)}`);
+      }
+      if (err instanceof woodError.NotExposeError) err.message = '服务器繁忙';
       if(res.print) {
         res.print(app.error(err));
       }else{
@@ -35,5 +43,11 @@ module.exports = (app = {}, config = {}) => {
       console.log('error when exit', e.stack);
     }
   });
+
+  process.on('unhandledRejection', (reason, p) => {
+    // 记录日志、抛出错误、或其他逻辑。
+    Log.error(`未处理的 rejection: ${reason.stack}`);
+  });
+  
   return app;
 }
